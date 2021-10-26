1bfe4be9ed177821cba6544d02bc0de4a04a4616
Gargant <gargant0373@gmail.com>
2021-10-26 22:24:07 +0300

    Update ItemBuildContainer.java

--- src/main/java/gargant/itembuilder/containers/ItemBuildContainer.java
+++ src/main/java/gargant/itembuilder/containers/ItemBuildContainer.java
@@ -1,14 +1,25 @@
 package gargant.itembuilder.containers;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 import masecla.mlib.containers.generic.ImmutableContainer;
 import masecla.mlib.main.MLib;
 
 public class ItemBuildContainer extends ImmutableContainer{
 
+	private Map<UUID, ItemStack> building = new HashMap<>();
+	
+	public void setBuilding(Player p, ItemStack s) {
+		this.building.put(p.getUniqueId(), s);
+	}
+	
 	public ItemBuildContainer(MLib lib) {
 		super(lib);
 	}
