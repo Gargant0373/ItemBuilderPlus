ede472b8a9cbf939e61dad24aedf7d5eff23fc38
Gargant <gargant0373@gmail.com>
2021-10-26 22:17:23 +0300

    Create ItemBuilderPlus.java

---
+++ src/main/java/gargant/itembuilder/main/ItemBuilderPlus.java
@@ -0,0 +1,24 @@
+package gargant.itembuilder.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import gargant.itembuilder.commands.ItemBuildCommand;
+import gargant.itembuilder.containers.ItemBuildContainer;
+import masecla.mlib.main.MLib;
+
+public class ItemBuilderPlus extends JavaPlugin{
+
+	private MLib lib;
+	
+	private ItemBuildContainer itemContainer;
+	
+	@Override
+	public void onEnable() {
+		this.lib = new MLib(this);
+		
+		this.itemContainer = new ItemBuildContainer(lib);
+		this.itemContainer.register();
+		
+		new ItemBuildCommand(lib, itemContainer).register();
+	}
+}
