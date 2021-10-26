9b2c0ce264ad22b3f8b4678394de0d7107f6c2ff
Gargant <gargant0373@gmail.com>
2021-10-26 22:24:00 +0300

    Added a building map and setBuilding method to keep track of the items people are building.

--- src/main/java/gargant/itembuilder/commands/ItemBuildCommand.java
+++ src/main/java/gargant/itembuilder/commands/ItemBuildCommand.java
@@ -1,20 +1,36 @@
 package gargant.itembuilder.commands;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
 import gargant.itembuilder.containers.ItemBuildContainer;
 import masecla.mlib.annotations.RegisterableInfo;
 import masecla.mlib.annotations.RequiresPlayer;
+import masecla.mlib.annotations.SubcommandInfo;
 import masecla.mlib.classes.Registerable;
 import masecla.mlib.main.MLib;
 
 @RegisterableInfo(command = "item")
 @RequiresPlayer
-public class ItemBuildCommand extends Registerable{
+public class ItemBuildCommand extends Registerable {
 
 	private ItemBuildContainer itemContainer;
-	
-	public ItemBuildCommand(MLib lib,ItemBuildContainer itemContainer) {
+
+	public ItemBuildCommand(MLib lib, ItemBuildContainer itemContainer) {
 		super(lib);
 		this.itemContainer = itemContainer;
+	}
+
+	@SubcommandInfo(subcommand = "open", permission = "itembuilder.build")
+	public void onOpen(Player p) {
+		ItemStack item = p.getInventory().getItemInMainHand();
+		if (item == null || item.getType().equals(Material.AIR)) {
+			lib.getMessagesAPI().sendMessage("exception.no-item", p);
+			return;
+		}
+		this.itemContainer.setBuilding(p, item);
+		lib.getContainerAPI().openFor(p, ItemBuildContainer.class);
 	}
 
 }
