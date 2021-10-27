3701dd7277acf48f4d54bf8bc4bb5d5493d13755
Gargant <gargant0373@gmail.com>
2021-10-28 00:20:00 +0300

    Added more commands to plugin.

--- src/main/java/gargant/itembuilder/commands/ItemBuildCommand.java
+++ src/main/java/gargant/itembuilder/commands/ItemBuildCommand.java
@@ -1,6 +1,7 @@
 package gargant.itembuilder.commands;
 
 import org.bukkit.Material;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -9,7 +10,9 @@ import masecla.mlib.annotations.RegisterableInfo;
 import masecla.mlib.annotations.RequiresPlayer;
 import masecla.mlib.annotations.SubcommandInfo;
 import masecla.mlib.classes.Registerable;
+import masecla.mlib.classes.Replaceable;
 import masecla.mlib.main.MLib;
+import net.md_5.bungee.api.ChatColor;
 
 @RegisterableInfo(command = "item")
 @RequiresPlayer
@@ -21,6 +24,21 @@ public class ItemBuildCommand extends Registerable {
 		super(lib);
 		this.itemContainer = itemContainer;
 	}
+	
+	@Override
+	public void fallbackCommand(CommandSender sender, String[] args) {
+		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&o&lItemBuilder+ &7- &f&oEasiest way to build items!"));
+		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&fRun /item &chelp &ffor help!"));
+		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7&oWritten with love by Gargant!"));
+	}
+	
+	@SubcommandInfo(subcommand = "help", permission = "itembuilder.help")
+	public void onHelp(Player p) {
+		p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&o&lItemBuilder+ &7- &fEasiest way to build items!"));
+		p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&f/item &chand &7- &fBuild the item in your hand!"));
+		p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&f/item &copen <Material> &7- &fBuild an item with the provided material."));
+		p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&f/item &creturn &7- &fReturn to building the previous item."));
+	}
 
 	@SubcommandInfo(subcommand = "open", permission = "itembuilder.build")
 	public void onOpen(Player p) {
@@ -31,6 +49,22 @@ public class ItemBuildCommand extends Registerable {
 		}
 		this.itemContainer.setBuilding(p, item.clone());
 		lib.getContainerAPI().openFor(p, ItemBuildContainer.class);
+	}
+	
+	@SubcommandInfo(subcommand = "open", permission = "itembuilder.build")
+	public void onOpenMaterial(Player p, String mat) {
+		Material m = Material.matchMaterial(mat);
+		if(m == null) {
+			lib.getMessagesAPI().sendMessage("exception.invalid-material", p, new Replaceable("%material%", mat));
+			return;
+		}
+		this.itemContainer.setBuilding(p, new ItemStack(m));
+		lib.getContainerAPI().openFor(p, ItemBuildContainer.class);
+	}
+	
+	@SubcommandInfo(subcommand = "hand", permission = "itembuilder.build")
+	public void onHand(Player p) {
+		this.onOpen(p);
 	}
 	
 	@SubcommandInfo(subcommand = "return", permission = "itembuilder.build")
